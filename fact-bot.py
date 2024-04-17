cb2e433d23ba672d37b01b1b99684629946dc4d0
Caleb Jackson <calebjackson2002@gmail.com>
2024-04-17 12:30:11 -0700

    initial commit with first layout of bot

---
+++ fact-bot.py
@@ -0,0 +1,44 @@
+import asyncio
+import datetime
+import discord
+import json
+import random as r
+import requests
+
+
+CHANNEL_ID = 1054646631066255432
+intents = discord.Intents.default()
+bot = discord.Client(intents=intents)
+
+
+# Setting up intents for bot to receive events related to 
+# server members and access content of messages.
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user}')
+
+async def get_message():
+    limit = 1
+    api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
+    response = requests.get(api_url, headers={'X-Api-Key': ''})
+    if response.status_code == requests.codes.ok:
+        data = json.loads(response.text)
+        for item in data:
+            print(f"Fact: {item['fact']}")
+    else:
+        print("Error:", response.status_code, response.text)
+
+    channel = bot.get_channel(CHANNEL_ID)
+    await channel.send(response.text)
+
+
+
+
+def run_bot(token):
+    bot.run(token)
