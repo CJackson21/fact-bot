1bd44b92fd166d1c4f40d5ed5c3045f5837832cc
Caleb Jackson <calebjackson2002@gmail.com>
2024-04-17 19:33:35 -0700

    added api secret file

--- fact-bot.py
+++ fact-bot.py
@@ -1,14 +1,18 @@
-import asyncio
-import datetime
 import discord
 import json
+import os
 import random as r
 import requests
+from discord.ext import tasks
+from dotenv import load_dotenv
+
 
 
 CHANNEL_ID = 1054646631066255432
 intents = discord.Intents.default()
 bot = discord.Client(intents=intents)
+load_dotenv()
+API_KEY= os.getenv('API_KEY')
 
 
 # Setting up intents for bot to receive events related to 
@@ -26,7 +30,7 @@ async def on_ready():
 async def get_message():
     limit = 1
     api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
-    response = requests.get(api_url, headers={'X-Api-Key': ''})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
     if response.status_code == requests.codes.ok:
         data = json.loads(response.text)
         for item in data:
@@ -37,8 +41,15 @@ async def get_message():
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send(response.text)
 
+@tasks.loop(minutes=20.0)
+async def send_fact():
+    if r.randint(0, 100) < 5:
+        get_message()
+
 
 
 
 def run_bot(token):
     bot.run(token)
+
+
